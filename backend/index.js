be639a7c5bf5df8fb9376cf667a3777082aa0350
riyubishnoi29 <bishnoiriya495@gmail.com>
2025-08-13 14:57:33 +0530

    Added SSL option for Aiven MySQL connection

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,      // from .env
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: true }
 });
 
 // --- API Routes ---
